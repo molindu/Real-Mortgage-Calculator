ba15529a3d4edfbd0cb9cfec2fd5dfefe4c8cc87
molindu <molinduyasodha@gmail.com>
2025-06-12 15:12:26 +0530

    initial changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Form from "./components/Form.tsx";
+import {useState} from "react";
+import Results from "./components/Results.tsx";
+import EmptyResults from "./components/EmptyResults.tsx";
+import type {CalculationResult} from "./type/types.ts";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [result, setResult] = useState<CalculationResult | null>(null)
+    return (
+        <main>
+            <div className={'flex bg-white sm:rounded-xl flex-col md:flex-row w-full sm:w-auto'}>
+                <Form setResult={setResult}/>
+                {result ? <Results result={result}/> : <EmptyResults/>}
+            </div>
+        </main>
+    )
 }
 
 export default App
